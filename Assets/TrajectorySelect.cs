7d5f2f9dbd0b85d6d68bb2a88ca4e8717ffa6b69
mlill <mtlillie@asu.edu>
2019-02-21 18:40:44 -0700

    first com

---
+++ Assets/TrajectorySelect.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+
+/**
+ * Created by: Matthew Lillie
+ * Date: February 8th, 2019
+ * 
+ * Handles the trajectory selection process for the satellite to be "launched" into the orbit of the asteroid. 
+ *
+ */
+public class TrajectorySelect : MonoBehaviour
+{
+    private float rotationSpeed = 30f;
+    private float rotationAngle = 60f;
+    public float startingRotation;
+    private bool stillMoving = true, positiveMovement = true;
+    public float currentAngle;
+
+    void Start()
+    {
+        startingRotation = transform.eulerAngles.z;
+        currentAngle = startingRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (stillMoving)
+        {
+            if(positiveMovement)
+            {
+                currentAngle = Mathf.MoveTowards(currentAngle, startingRotation + rotationAngle,
+                                rotationSpeed * Time.deltaTime);
+                if(currentAngle >= startingRotation + rotationAngle)
+                {
+                    positiveMovement = false;
+                }
+                transform.eulerAngles = new Vector3(0, 0, currentAngle);
+            } else
+            {
+                currentAngle = Mathf.MoveTowards(currentAngle, startingRotation - rotationAngle,
+                                rotationSpeed * Time.deltaTime);
+                if (currentAngle <= startingRotation - rotationAngle)
+                {
+                    positiveMovement = true;
+                }
+                transform.eulerAngles = new Vector3(0, 0, currentAngle);
+            }
+  
+        }
+    }
+
+    void OnMouseDown()
+    {
+        stillMoving = !stillMoving;
+    }
+}
